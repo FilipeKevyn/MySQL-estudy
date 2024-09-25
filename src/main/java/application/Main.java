8c0e53b683821eebacde4462615a2dffa235d72b
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-09-24 22:08:34 -0300

    Main

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,2 +1,18 @@
-package application;public class Main {
+package application;
+
+import db.DB;
+import model.dao.DAOFactory;
+import model.dao.VendedorDAO;
+import model.entidades.Vendedor;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        VendedorDAO vendedorDAO = DAOFactory.criarVendedorDAO();
+
+        Vendedor vendedor = vendedorDAO.encontrarById(3);
+
+        System.out.println(vendedor);
+    }
 }
