d9f9c526f705645300cd296f21a32cba95f20383
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-10-01 16:33:52 -0300

    Instanciar DepartamentoDAO

--- src/main/java/model/dao/DAOFactory.java
+++ src/main/java/model/dao/DAOFactory.java
@@ -6,4 +6,7 @@ public class DAOFactory {
     public static VendedorDAO criarVendedorDAO(){
         return new VendedorDAO(DB.getConnection());
     }
+    public static DepartamentoDAO criarDepartamentoDAO(){
+        return new DepartamentoDAO(DB.getConnection());
+    }
 }
